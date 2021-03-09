2f13181666ce9727f2ef893f7dfa83f007cf76d7
Erick Avila <nmr.inc@icloud.com>
2021-03-08 19:21:19 -0600

    Header creation

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from '../components/Header';
 
 const Layout = ({ children }) => {
   return (
     <div className="container-fluid">
+      <Header />
       {children}
     </div>
   )
